8c85e2ddc93f1511c029a599bc70561f5810da80
KhiemNguyen15 <khiemsa15@gmail.com>
2024-05-28 20:12:11 -0500

    style: Add newline to build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ tasks.test {
     useJUnitPlatform()
     jvmArgs("--enable-preview")
 }
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
